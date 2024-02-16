1375a137ec0b9fb9155a428d03bea0a87b9958b5
Rahul <rahul>
2024-02-17 00:15:57 +0530

    database

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
 
-// const DB = 'mongodb+srv://rg3427gr:Rahul@123@cluster0.hteigdr.mongodb.net/users?retryWrites=true&w=majority'
+// const DB =
+//   "mongodb+srv://rg3427gr:Rahul@123@cluster0.hteigdr.mongodb.net/users?retryWrites=true&w=majority";
 // const DB = 'mongodb+srv://rohan8933:Rahulgautam8933@cluster0.1s3qk9i.mongodb.net/collections?retryWrites=true&w=majority'
-// const DB = 'mongodb+srv://vikasroshni8:vikas@123@cluster0.mve7ka4.mongodb.net/roshni_opticals?retryWrites=true&w=majority'
-// const DB = 'mongodb+srv://vikasroshni8:kkyVTkEzHaKif74M@cluster0.mve7ka4.mongodb.net/roshni1?retryWrites=true&w=majority'
+// const DB =
+//   "mongodb+srv://vikasroshni8:vikas@123@cluster0.mve7ka4.mongodb.net/roshni_opticals?retryWrites=true&w=majority";
+// const DB =
+//   "mongodb+srv://vikasroshni8:kkyVTkEzHaKif74M@cluster0.mve7ka4.mongodb.net/roshni1?retryWrites=true&w=majority";
 // const DB =
 //   "mongodb+srv://vikasroshni8:kkyVTkEzHaKif74M@cluster0.opaefkh.mongodb.net/roshni?retryWrites=true&w=majority";
-const DB = "mongodb://localhost:27017/school";
+// const DB = "mongodb://localhost:27017/school";
+const DB =
+  "mongodb+srv://ankitroyal74339:Rahul%40123@cluster0.htm4s2b.mongodb.net/ankitroyal?retryWrites=true&w=majority";
+
+// const DB =
+//   "mongodb+srv://vikasroshni8:kkyVTkEzHaKif74M@cluster0.opaefkh.mongodb.net/roshni?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB)
